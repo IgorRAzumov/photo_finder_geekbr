87d52fe86b0230561e466126db3af190f6d46e19
Igor <1itcode1@gmail.com>
2018-03-06 15:47:31 +0300

    delete menu item, change app bar search icon

--- app/src/main/java/ru/geekbrains/photofinder/activities/MapActivity.java
+++ app/src/main/java/ru/geekbrains/photofinder/activities/MapActivity.java
@@ -74,10 +74,6 @@ public class MapActivity extends AppCompatActivity implements
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_settings: {
-                actionSettingsClick();
-                return true;
-            }
             case R.id.action_search_Address: {
                 actionSearchClick();
                 return true;
